2fd61e9143c5bcd02208ee375ce0be0bea93f170
GreXLin85 <grexlin85@gmail.com>
2023-07-06 21:25:12 +0300

    types added to package

---
+++ src/index.d.ts
@@ -0,0 +1,34 @@
+type Thread = {
+    id: String,
+    content: String,
+    user: String,
+    whenShared: Date,
+    repliesCount: Number,
+    likesCount: Number
+}
+
+declare module 'threadsnetapi' {
+    export class ThreadsAPI {
+        constructor({
+            proxy,
+            headless
+        }: {
+            proxy?: import("playwright").LaunchOptions["proxy"] | undefined;
+            headless?: boolean | undefined;
+        });
+        checkIfInitialized(): void;
+        init(): Promise<void>;
+        close(): Promise<void>;
+        getThread(threadId?: String): Promise<Thread>;
+        getUser(userId?: String): Promise<{
+            id: String;
+            name: String;
+            username: String;
+            bio: String;
+            profilePicture: String;
+            followersCount: Number;
+            instagramAccount: String;
+        }>;
+        getUserThreads(userId?: String): Promise<Thread[]>;
+    }
+}
